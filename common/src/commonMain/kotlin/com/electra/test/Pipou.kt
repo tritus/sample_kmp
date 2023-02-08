90b2117a16f30e5e6e3267d4e07eff077ac061ed
Tristan Ferré <tristan@go-electra.com>
2023-02-09 00:04:51 +0100

    wip nav

--- common/src/commonMain/kotlin/com/electra/test/Pipou.kt
+++ common/src/commonMain/kotlin/com/electra/test/Pipou.kt
@@ -1,5 +1,40 @@
 package com.electra.test
 
-class Pipou {
-    val pipo = "wow"
+class FirstViewModel(
+    private val navController: NavController
+) {
+    fun onCLick() {
+        navController.push(NavLink.SECOND)
+    }
+}
+
+class SecondViewModel(
+    private val navController: NavController
+) {
+    fun onCLick() {
+        navController.push(NavLink.THIRD)
+    }
+}
+
+class ThirdViewModel(
+    private val navController: NavController
+) {
+    fun onCLick() {
+        navController.popTo(NavLink.FIRST)
+    }
+}
+
+enum class NavLink {
+    FIRST,
+    SECOND,
+    THIRD
+}
+
+object AppConstants {
+    val rootScreen = NavLink.FIRST
+}
+
+interface NavController {
+    fun push(navLink: NavLink)
+    fun popTo(navLink: NavLink)
 }
--- iosApp/iosApp/iosAppApp.swift
+++ iosApp/iosApp/iosAppApp.swift
@@ -6,12 +6,129 @@
 //
 
 import SwiftUI
+import common
 
 @main
 struct iosAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ElectraNavigationView(
+                content: { navController in
+                    AnyView(FirstScreen(navController: navController))
+                }
+            )
         }
     }
+}
+
+struct ElectraNavigationView: View {
+    let content: (NavController) -> AnyView
+    @State var destination: NavLink? = nil
+    var body: some View {
+        NavigationView(
+            content: {
+                let navController = IOSNavController(onPush: { destination = $0 }, onPop: { _ in})
+                content(navController)
+                if let navLink = destination {
+                    NavigationLink(
+                        "",
+                        isActive: .init(get: { destination != nil }, set: { _ in }),
+                        destination: {
+                            ElectraNavigationView(
+                                content: { childNavController in
+                                    destinationFor(navLink, navController: childNavController)
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        )
+    }
+}
+
+struct FirstScreen: View {
+    private let viewmodel: FirstViewModel
+
+    init(navController: NavController) {
+        self.viewmodel = FirstViewModel(navController: navController)
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("1!")
+            Button("Click me to go to 2!", action: { viewmodel.onCLick() })
+        }
+        .padding()
+    }
+}
+
+struct SecondScreen: View {
+    private let viewmodel: SecondViewModel
+
+    init(navController: NavController) {
+        self.viewmodel = SecondViewModel(navController: navController)
+    }
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("2!")
+            Button("Click me to go to 3!", action: { viewmodel.onCLick() })
+        }
+        .padding()
+    }
+}
+
+struct ThirdScreen: View {
+    private let viewmodel: ThirdViewModel
+
+    init(navController: NavController) {
+        self.viewmodel = ThirdViewModel(navController: navController)
+    }
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("3!")
+            Button("Click me to go back to 1!", action: { viewmodel.onCLick() })
+        }
+        .padding()
+    }
+}
+
+class IOSNavController: NavController {
+    let onPush: (NavLink) -> Void
+    let onPop: (NavLink) -> Void
+    
+    init(onPush: @escaping (NavLink) -> Void, onPop: @escaping (NavLink) -> Void) {
+        self.onPush = onPush
+        self.onPop = onPop
+    }
+    
+    func popTo(navLink: NavLink) {
+        onPop(navLink)
+    }
+    
+    func push(navLink: NavLink) {
+        onPush(navLink)
+    }
+}
+
+func destinationFor(_ navLink: NavLink, navController: NavController) -> AnyView {
+    let destination: any View
+    switch navLink {
+    case NavLink.first: destination = FirstScreen(navController: navController)
+    case NavLink.second: destination = SecondScreen(navController: navController)
+    case NavLink.third: destination =  ThirdScreen(navController: navController)
+    default: fatalError("navlink unknown")
+    }
+    return AnyView(destination)
 }
